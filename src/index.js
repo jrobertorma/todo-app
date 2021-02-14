5930ea3a8398f9c103bc4b51a7fef9ca4887acfe
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-02-14 14:15:38 -0600

    Adding firebase support through react context api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

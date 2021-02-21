b5ea292e371a2c0e6cee4b7b32a4d6fe39e6ca5d
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-02-21 09:19:10 -0600

    Adding signIn components

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,42 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { SignUpLink } from '../SignUp';
+
+import { withFirebase } from '../Firebase';
+
+import * as ROUTES from '../../constants/routes';
+
 const SingInPage = () => {
     return(
         <div>
-            YOYOYO I'M SIGNIN BRO
+            <h1>SignIn</h1>
+            <SignInForm />
+            <SignUpLink />
         </div>
     );
 }
- 
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null,
+};
+
+class SignInFormBase extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = (event) => {
+
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+}
+
 export default SingInPage;

188822587b838f1cd6d7ff2c16ae39cbabbcf635
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-02-22 18:44:51 -0600

    Adding SignIn components

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -12,6 +12,22 @@ export default FirebaseContext;
 
 /**
  * Create the Firebase context so we don't have to create multiple firebase instances, this file is called by Firebase/index.js
- * note the withFirebase component, it recieves a component as a prop and then re-render it wrapped between the FirebaseContext
- * this allow any component to use the firebase context, without needing any parent component to pass it before
+ * note the withFirebase component, it recieves a component as a prop and then re-renders it wrapped between the FirebaseContext
+ * this allow any component to use the firebase context, without needing any parent component to pass it before by simply calling it
+ * with the 'withFirebase' function like this:
+ * 
+ * withFirebase(SignUpFormBase)
+ * 
+ * SignUpFormBase, now can access firebase functions like:
+ * 
+ * this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
+              })
+              .catch(error => {
+                  this.setState({ error });
+              });
+ * 
  */

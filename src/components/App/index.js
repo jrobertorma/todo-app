7562d9e6f04b3d2f3a079f4ed252b71a92531f33
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-02-24 08:27:51 -0600

    Adding session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,27 @@
-import React from "react";
-import Navigation from '../Navigation'
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      authUser: null,
+    }
+  }
+  render() { 
+    return ( 
+      <Router>
+        <div className="App">
+          <Navigation authUser={this.state.authUser} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
+/**
+ * Since we are not going to use a state management library, the user's sessions will be stored in the react
+ * state of the App component (see the 8th line).
+ */

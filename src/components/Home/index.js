2d34b74d833eeb5ef1468f9a073292804e8a8782
J. Roberto Ramirez <jrobertorma@gmail.com>
2021-03-11 08:58:08 -0600

    Adding route protection

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,34 @@
+import React from 'react';
+
+import { withAuthorization } from '../Session';
+
 const HomePage = () => {
     return ( 
         <div>
-            YOYOYO, I'M HOMEEEE
+            <h1>Home Page</h1>
+            <p>The Home Page is accessible by every signed in user.</p>
         </div>
      );
 }
+
+const condition = (authUser) => !!authUser; //(false false) expression, returns the 'truthiness' of its argument
  
-export default HomePage;
+export default withAuthorization(condition)(HomePage);
+
+/**
+ * The HomePage component (really? lol)
+ * 
+ * The most atypical thing about this component is the way we create the 'condition' function.
+ * It receives an authUser state and returns !!authUser (false false? state).
+ * 
+ * This is a kind of obscure way to do a type conversion.
+ * 
+ * The 'false false' thing (it is not an operator per se) returns true or false depending on the 'truthiness' of the expression.
+ * e.g.
+ * 
+ * !!"" === false // empty string is falsy
+ * !!"foo" === true  // non-empty string is truthy
+ * !!"false" === true  // ...even if it contains a falsy value
+ * 
+ * see the thread on https://stackoverflow.com/questions/784929/what-is-the-not-not-operator-in-javascript to know more.
+ */

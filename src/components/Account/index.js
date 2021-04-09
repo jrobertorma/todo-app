d59cec0cbb9878a486069c37751a53ec82dde3ee
jrobertorma <jrobertorma@gmail.com>
2021-04-09 09:59:57 -0500

    Linking social accounts

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { AuthUserContext, withAuthorization } from '../Session';
+import { withFirebase } from '../Firebase';
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
@@ -39,28 +40,59 @@ const AccountPage = () => {
      );
 }
 
-class LoginManagement extends Component {
+class LoginManagementBase extends Component {
     constructor(props) {
         super(props);
-        // this.state = {  }
+        this.state = { 
+            activeSignInMethods: [],
+            error: null,
+         };
     }
-    render() { 
+
+    componentDidMount() {
+        this.props.firebase.auth
+            .fetchSignInMethodsForEmail(this.props.authUser.email)
+            .then( (activeSignInMethods) => {
+                this.setState({ activeSignInMethods, error: null })
+            })
+            .catch( (error) => this.setState({ error }) )
+    }
+
+    render() {
+        const { activeSignInMethods, error } = this.state;
+
         return ( 
             <div>
                 Sign In Methods:
                 <ul>
                     {SIGN_IN_METHODS.map( (signInMethod) => {
-                        return( <li key={signInMethod.id}>
+                        const isEnabled = activeSignInMethods.includes(
+                            signInMethod.id,
+                        );
+
+                        return( 
+                            <li key={signInMethod.id}>
+                                {
+                                    isEnabled ? (
                                     <button type="button" onClick={ () => {} }>
-                                        {signInMethod.id}
-                                    </button>
-                                </li> );
+                                        Deactivate {signInMethod.id}
+                                    </button> ): (
+                                    <button type="button" onClick={ () => {} }>
+                                        Link {signInMethod.id}
+                                    </button> )
+                                }
+                            </li> );
                     } )}
                 </ul>
+                
+                {error && error.message}
+
             </div>
         );
     }
 }
+
+const LoginManagement = withFirebase (LoginManagementBase);
 
 const condition = authUser => !!authUser; // '!!expression', returns the 'truthiness' of expression
 
@@ -71,10 +103,10 @@ export default withAuthorization (condition)(AccountPage);
  * 
  * This component displays the user's mail and two forms to reset his/her's password, via mail reset or by typing the new password.
  * 
- * See how it is exported: using the withAuthorization HOC created at src\components\Session\withAuthorization.js (line 28).
+ * See how it is exported: using the withAuthorization HOC created at src\components\Session\withAuthorization.js (line 67).
  * 
- * That component checks if the user is logged in (using the 'condition' function, line 26), and depending on that calls the second
- * parameter (AccountPage in this case, line 28).
+ * That component checks if the user is logged in (using the 'condition' function, line 65), and depending on that calls the second
+ * parameter (AccountPage in this case, line 65).
  * 
  * Note how the component uses the 'authUser' object. That is possible because the withAuthorization HOC, passes it as a prop at some
  * point (see the HOC's notes).

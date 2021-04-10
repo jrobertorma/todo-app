70d1f3909f4fbbfb746151de52af20b0d01cc69f
jrobertorma <jrobertorma@gmail.com>
2021-04-10 14:54:34 -0500

    Linking social accounts

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -50,12 +50,30 @@ class LoginManagementBase extends Component {
     }
 
     componentDidMount() {
+        this.fetchSignInMethods();
+    }
+
+    fetchSignInMethods = () => {
         this.props.firebase.auth
             .fetchSignInMethodsForEmail(this.props.authUser.email)
             .then( (activeSignInMethods) => {
                 this.setState({ activeSignInMethods, error: null })
             })
             .catch( (error) => this.setState({ error }) )
+    }
+
+    onSocialLoginLink = (provider) => {
+        this.props.firebase.auth.currentUser
+            .linkWithPopup(this.props.firebase[provider])
+            .then(this.fetchSignInMethods)
+            .catch(error => this.setState({ error }))
+    }
+
+    onUnlink = (providerId) => {
+        this.props.firebase.auth.currentUser
+            .unlink(providerId)
+            .then(this.fetchSignInMethods)
+            .catch(error => this.setState({ error }))
     }
 
     render() {
@@ -66,6 +84,7 @@ class LoginManagementBase extends Component {
                 Sign In Methods:
                 <ul>
                     {SIGN_IN_METHODS.map( (signInMethod) => {
+                        const onlyOneLeft = activeSignInMethods.length === 1;
                         const isEnabled = activeSignInMethods.includes(
                             signInMethod.id,
                         );
@@ -74,10 +93,17 @@ class LoginManagementBase extends Component {
                             <li key={signInMethod.id}>
                                 {
                                     isEnabled ? (
-                                    <button type="button" onClick={ () => {} }>
+                                    <button 
+                                        type="button" 
+                                        onClick={ () => this.onUnlink(signInMethod.id) }
+                                        disabled={onlyOneLeft}
+                                    >
                                         Deactivate {signInMethod.id}
                                     </button> ): (
-                                    <button type="button" onClick={ () => {} }>
+                                    <button 
+                                        type="button" 
+                                        onClick={ () => this.onSocialLoginLink(signInMethod.provider) }
+                                    >
                                         Link {signInMethod.id}
                                     </button> )
                                 }

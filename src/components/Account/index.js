732f4de2fa0bfa4dcc17c5d53b619c631f010553
jrobertorma <jrobertorma@gmail.com>
2021-04-12 08:58:10 -0500

    Linking social accounts

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -125,6 +125,75 @@ class LoginManagementBase extends Component {
     }
 }
 
+const SocialLoginToggle = ({
+    onlyOneLeft,
+    isEnabled,
+    signInMethod,
+    onLink,
+    onUnlink,
+}) => {
+    isEnabled ? (
+        <button 
+            type="button"
+            onClick={() => onUnlink(signInMethod.id)}
+            disabled={onlyOneLeft}
+        >
+            Deactivate {signInMethod.id}
+        </button>
+    ) : (
+        <button
+            type="button"
+            onClick={() => onLink(signInMethod.provider)}
+        >
+            Link {signInMethod.id}
+        </button>
+    )
+}
+
+class DefaultLoginToggle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { passwordOne: '', passwordTwo: '' }
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onLink(this.state.passwordOne);
+        this.setState({ passwordOne: '', passwordTwo: '' });
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    render() { 
+        const {
+            onlyOneLeft,
+            isEnabled,
+            signInMethod,
+            onUnlink,
+        } = this.props;
+
+        const { passwordOne, passwordTwo } = this.state;
+
+        const isInvalid =
+            passwordOne !== passwordTwo || passwordOne === '';
+
+        return ( 
+            isEnabled ? (
+                <button>
+
+                </button>
+            ) : (
+                <form>
+                    
+                </form>
+            ) 
+        );
+    }
+}
+
 const LoginManagement = withFirebase (LoginManagementBase);
 
 const condition = authUser => !!authUser; // '!!expression', returns the 'truthiness' of expression

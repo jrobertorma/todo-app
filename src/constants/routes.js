9e3dadb4805612f976400fa6ff80ad73971f6d9a
jrobertorma <jrobertorma@gmail.com>
2021-05-01 21:53:30 -0500

    Adding admin dashboard

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,9 +5,16 @@ export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
+export const ADMIN_DETAILS = '/admin/:id'
 
 /**
  * We use this constants to provide a standarized set of routes for the app
  * if you need to change any route, you only need to change the const value instead of all
  * the files that use the route.
+ * 
+ * Notice the ADMIN_DETAILS const. It is a child route, the ':id' string is a placeholder for a user id that we will use later.
+ * We could have been mor specific and define a route like: '/admin/users/:id', maybe we will manage 
+ * other entities in the admin page. e.g., the app will store a set of users and the books written by them, 
+ * then it will make sense to have detail pages for users (/admin/users/:userId) and books (/admin/books/:bookId).
+ * 
  */

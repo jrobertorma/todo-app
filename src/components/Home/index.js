db8fa9acfc5649bf46dab6c4cec7db3306f44aad
jrobertorma <jrobertorma@gmail.com>
2021-05-09 07:54:12 -0500

    Adding Notes functionalities

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { withFirebase } from '../Firebase';
 
 import { withAuthorization, withEmailVerification } from '../Session';
 
@@ -7,9 +8,49 @@ const HomePage = () => {
         <div>
             <h1>Home Page</h1>
             <p>The Home Page is accessible by every signed in user.</p>
+            
+            <Notes />
         </div>
      );
 }
+
+class NotesBase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            notes: [],
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({ loading: true });
+
+        this.props.firebase.notes().on('value', snapshot => {
+            //convert notes list from snapshot (is a set of objects)
+
+            this.setState({ loading: false });
+        });
+    }
+
+    componentWillUnmount() {
+        this.props.firebase.notes.off();
+    }
+
+    render() { 
+        const { notes, loading } = this.state;
+
+        return (
+            <div>
+                { loading && <div>Loading ...</div> }
+
+                Soy una lista de notas
+            </div>
+        );
+    }
+}
+
+const Notes = withFirebase(NotesBase);
 
 const condition = (authUser) => !!authUser; //'!!expression', returns the 'truthiness' of expression
  
@@ -33,4 +74,15 @@ export default withEmailVerification(withAuthorization(condition)(HomePage));
  * !!"false" === true  // ...even if it contains a falsy value
  * 
  * see the thread on https://stackoverflow.com/questions/784929/what-is-the-not-not-operator-in-javascript to know more.
+ * 
+ * HomePage also renders a list of notes for the user
+ * 
+ * Notice the componentDidMount() event in the NotesBase component.
+ * 
+ * It adds a firebase 'listener' to the 'notes' entity that will call the fallback function every time the db changes.
+ * 
+ *      this.props.firebase.notes().on( value => //to do )
+ * 
+ * And because it has a setState function it will re-render the changed components.
+ * 
  */

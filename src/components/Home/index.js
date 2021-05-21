a3d7de4095caedbc6357879b2982c60e7a834805
jrobertorma <jrobertorma@gmail.com>
2021-05-21 08:08:25 -0500

    Adding todoList functionalities

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -75,8 +75,22 @@ class TodoListBase extends Component {
         event.preventDefault();
     }
 
+    /**
+     * Notice that todoItem(uid) !== todoItems(uid), the first one (defined at src\components\Firebase\firebase.js)
+     * has the placeholder for the uid var and pass it to the firebase ref:
+     * 
+     *  todoItem = uid => this.db.ref(`todoItems/${uid}`);
+     * 
+     * todoItems() doesn't
+     * 
+     *  todoItems = () => this.db.ref('todoItems');
+     */
     onRemoveItem = uid => {
-        this.props.firebase.todoItems(uid).remove();
+        this.props.firebase.todoItem(uid).remove();
+    }
+
+    onEditItem = () => {
+
     }
 
     render() {
@@ -92,6 +106,7 @@ class TodoListBase extends Component {
                             todoListItems ? (
                                 <ItemsList 
                                     todoListItems={todoListItems}
+                                    onEditItem={this.onEditItem}
                                     onRemoveItem={this.onRemoveItem}
                                 />
                             ) : (
@@ -114,20 +129,19 @@ class TodoListBase extends Component {
     }
 }
 
-const ItemsList = ({ todoListItems, onRemoveItem }) => {
+const ItemsList = ({ todoListItems, onEditItem, onRemoveItem }) => {
     return (
         <ul>
             {   /*the map is important, as it is to get the todoListItems as a function param, not
                 * using this.props, every listItem object (todoListItems is an array of objects)
                 * has the 'text', 'userId' and 'uid' keys.
-                * 
-                * listItem.text
                 */
                 todoListItems.map( listItem => {
                     return(
                         <ListItem 
                             key={listItem.uid}
                             listItem={listItem}
+                            onEditItem={onEditItem}
                             onRemoveItem={onRemoveItem}
                         />
                     )
@@ -137,18 +151,68 @@ const ItemsList = ({ todoListItems, onRemoveItem }) => {
     );
 }
 
-const ListItem = ({ listItem, onRemoveItem }) => {
-    return (
-        <li>
-            <strong>{listItem.uid}</strong> {listItem.text}
-            <button
-                type="button"
-                onClick={ () => onRemoveItem(listItem.uid) }
-            >
-                Delete
-            </button>
-        </li>
-    );
+// const ListItem = ({ listItem, onRemoveItem }) => {
+//     return (
+//         <li>
+//             <strong>{listItem.uid}</strong> {listItem.text}
+//             <button
+//                 type="button"
+//                 onClick={ () => onRemoveItem(listItem.uid) }
+//             >
+//                 Delete
+//             </button>
+//         </li>
+//     );
+// }
+
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            editMode: false,
+            editText: this.props.listItem.text,
+        };
+    }
+
+    onToggleEditMode = () => {
+        this.setState( state => ({
+            editMode: !state.editMode,
+            editText: this.props.listItem.text,
+        }));
+    }
+
+    onChangeEditText = event => {
+        this.setState({ editText: event.target.value });
+    }
+
+    onSaveEditText = () => {
+        this.props.onEditItem(this.props.listItem, this.state.editText);
+
+        this.setState({ editMode: false });
+    }
+
+    render() { 
+        const { listItem, onRemoveItem } = this.props;
+        const { editMode, editText } = this.state;
+
+        return ( 
+            <li>
+                <span>
+                    <strong>{listItem.uid}</strong> {listItem.text}
+                </span>
+                
+                { editMode && (
+                    <button
+                        type="button"
+                        onClick={ () => onRemoveItem(listItem.uid) }
+                    >
+                        Delete
+                    </button>
+                )}
+            </li>
+        );
+    }
 }
 
 const TodoList = withFirebase(TodoListBase);
@@ -180,36 +244,35 @@ export default compose(
  * 
  * see the thread on https://stackoverflow.com/questions/784929/what-is-the-not-not-operator-in-javascript to know more.
  * 
- * HomePage also renders a list of notes for the user
+ * HomePage also renders a todo-list for the user
  * 
  * Notice the componentDidMount() event in the NotesBase component.
  * 
- * It adds a firebase 'listener' to the 'notes' entity that will call the fallback function every time the db changes.
+ * It adds a firebase 'listener' to the 'todo-list items' entity that will call the fallback function every time the db changes.
  * 
- *      this.props.firebase.notes().on( value => //to do )
+ *      this.props.firebase.todoItems().on( value => //to do )
  * 
  * And because it has a setState function it will re-render the changed components 
  * (https://firebase.google.com/docs/database/web/read-and-write#web_value_events).
  * 
- * It is important to note that .notes() is an API function defined at src\components\Firebase\firebase.js
+ * It is important to know that .todoItems() is an API function defined at src\components\Firebase\firebase.js
  * 
- *      // *** Notes API ***
- *      note = uid => this.db.ref(`notes/${uid}`);
- *      notes = () => this.db.ref('notes');
+ *      // *** To-do list API ***
+ *      todoItem = uid => this.db.ref(`todoItems/${uid}`);
+ *      todoItems = () => this.db.ref('todoItems');
  * 
  * So the listener will be the same as:
  *      
- *      firebase.db.ref('notes').on( snapshot => //todo )
+ *      firebase.db.ref('todoItems').on( snapshot => //todo )
  * 
  * Just like the docs suggest.
  * 
- * The same component also displays a form to save new notes, the onSubmit handler, calls the onCreateNote function,
- * it calls the firebase's .push() method (https://firebase.google.com/docs/database/admin/save-data) and cleans the input
+ * The same component also displays a form to save new todo-list items, the onSubmit handler, calls the onCreateNote function,
+ * that then calls the firebase's .push() method (https://firebase.google.com/docs/database/admin/save-data) and cleans the input
  * state (a controlled form), that method returns an id for the new record in the db. The <NotesBase> component also uses
  * the authUser context to get the uid of the logged in user (see its render function), and be able to pass it to the 
  * onCreateNote function, a random fact is that when you have several parameters to pass in a function, as a form handler, 
- * you must pass the event first:
+ * you must pass the 'event' parameter first:
  * 
  *      event => this.onCreateNote(event, authUser)
- * 
  */
